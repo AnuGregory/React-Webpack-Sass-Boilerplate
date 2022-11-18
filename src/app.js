6cd967f9276321e5b2a6b293e080cfe5d1b6d0d3
Anu Gregory <anugregory007@gmail.com>
2022-11-18 13:22:59 +0100

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.scss';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p className={'test'}>this is scss test</p>
+        </div>
+    );
+};
+
+export default App;
